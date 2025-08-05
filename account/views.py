979f6fb16e8ccdec12252ec7439cc313416db034
TanishaBhamare <tanishabhamare10@gmail.com>
2025-08-06 00:13:44 +0530

    18375

--- account/views.py
+++ account/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ContactForm
 
-# Create your views here.
+def homepage(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request,'contact/home.html',{
+                'form':ContactForm(),
+                'success': True
+            })
+        else:
+            form = ContactForm()
+        return render(request, 'contact/home.html' ,{'form':form})        
+
+
