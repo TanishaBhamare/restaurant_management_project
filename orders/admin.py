6e7f0eb6fe67c5d2108c3fb2d87ec052e4c3e26d
TanishaBhamare <tanishabhamare10@gmail.com>
2025-08-05 23:46:17 +0530

    18373

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Order,OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+class OrderItemline(admin.TabularInLine):
+    model = OrderItem
+    extra = 0
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','customer','total_amount','order_status','created_at')
+    list_filter = ('order_status','created_at')
+    search_fields = ('customer__username',)
+    inlines = [OrderItemline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order','menu_item','quantity')   
