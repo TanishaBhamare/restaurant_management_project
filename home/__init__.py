9101778d0c079a9f0b1640a8168505b5d74e71f9
TanishaBhamare <tanishabhamare10@gmail.com>
2025-08-06 00:22:34 +0530

    18375

---
+++ home/__init__.py
@@ -0,0 +1,15 @@
+#home.html
+<html>
+<head>
+     <title> Contact us </title>
+</head>
+<body>
+    <h1>Contact Us</h1>
+
+      <p style="color:green;">Thank you! Your  information has been submitted.</p>
+
+   <form method="post">
+   <button type="submit">Submit</button>
+   </form>
+<body>
+</html>   
