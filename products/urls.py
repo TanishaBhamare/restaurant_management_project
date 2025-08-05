9d69f18be898a359a889acd859d07fdf692ab21c
TanishaBhamare <tanishabhamare10@gmail.com>
2025-08-06 00:17:15 +0530

    18375

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from django.urls import path,include
 
 urlpatterns = [
-    path('items/', ItemView.as_view(), name='item-list'),
+    path('admin/', admin.site.urls),
+    path('',include('contact.urls')),
 ]
