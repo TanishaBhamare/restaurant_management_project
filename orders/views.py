dd0b84f0d41b19bf8257a94261ae2f91a2b2ba9d
TanishaBhamare <tanishabhamare10@gmail.com>
2025-08-05 22:38:16 +0530

    18371

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def homepage(request):
+    try:
+        response = request.get('http://localhost:8000/api/menu/')
+        manu_items = response.json()
+    except Exception as e:
+        manu_items = []
+    return render(request, 'restaurant/manu.html',{'menu_items':menu_items})        
