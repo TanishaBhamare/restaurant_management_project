301499508b4b9b1c5fb8a5231a65c2ac46450616
TanishaBhamare <tanishabhamare10@gmail.com>
2025-08-06 22:55:23 +0530

    18378

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Menu
-from .models import MenuItem
+from .models import Menu, MenuItem
+
 
 @admin.register(Menu)
-admin.site.register
 class MenuAdmin(admin.ModelAdmin):
     list_display('name','price'):
     search_fields = ('name')
     list_filter = ('price')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name','price')
+    search_fields = ('name',)
+    list_filter = ('price',)
